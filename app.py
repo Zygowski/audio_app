2a92df0b993bec3a191c052f20376b9623e576a7
Mikołaj <mikolajarski@gmail.com>
2025-06-12 19:22:12 +0200

    d

--- app.py
+++ app.py
@@ -3,11 +3,17 @@ import streamlit as st
 st.set_page_config(page_title="Audio Summarizer V2", layout="centered")
 st.title("Generetor notatek z audio")
 
-uploaded_file = st.file_uploader("Wybierz plik audio", type=["mp3", "wav", "m4a"])
-uploaded_file = st.file_uploader("Wybierz plik wideo", type=["mp4", "mov", "avi", "mkv"])
-if uploaded_file is not None:
-    st.success(f"Wczytano plik: {uploaded_file.name}")
+file_type = st.radio("Wybierz typ pliku:", ("Audio", "Wideo"))
+if file_type == "Audio":
+    uploaded_audio = st.file_uploader("Wybierz plik audio", type=["mp3", "wav", "m4a"])
+    
+    if uploaded_audio is not None:
+        st.success(f"Wczytano plik audio: {uploaded_audio.name}")
+        st.audio(uploaded_audio, format="audio/mp3")
 
-    # Odtwarzanie audio bez zapisu
-    st.audio(uploaded_file, format="audio/mp3")
-    st.video(uploaded_file)
+elif file_type == "Wideo":
+    uploaded_video = st.file_uploader("Wybierz plik wideo", type=["mp4", "mov", "avi", "mkv"])
+    
+    if uploaded_video is not None:
+        st.success(f"Wczytano plik wideo: {uploaded_video.name}")
+        st.video(uploaded_video)
