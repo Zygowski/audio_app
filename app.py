87ea645c9695c9a0d7980f49518be36c06c69aeb
Mikołaj <mikolajarski@gmail.com>
2025-06-12 19:28:44 +0200

    s

--- app.py
+++ app.py
@@ -20,3 +20,18 @@ elif file_type == "Wideo":
     if uploaded_video is not None:
         st.success(f"Wczytano plik wideo: {uploaded_video.name}")
         st.video(uploaded_video)
+                # Ekstrakcja audio
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as temp_video:
+            temp_video.write(uploaded_video.read())
+            temp_video_path = temp_video.name
+
+        video_clip = VideoFileClip(temp_video_path)
+        audio_clip = video_clip.audio
+
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp_audio:
+            audio_clip.write_audiofile(temp_audio.name)
+            temp_audio.seek(0)
+            st.audio(temp_audio.read(), format="audio/mp3")
+
+        audio_clip.close()
+        video_clip.close()
