a390a2db3bf68e7015bb69af87d084a9854107da
Mikołaj <mikolajarski@gmail.com>
2025-06-15 11:56:31 +0200

    s

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ from moviepy.editor import VideoFileClip
 import tempfile
 import os
 import yt_dlp
-from pytube import YouTube
-from pydub import AudioSegment
+
 
 st.set_page_config(page_title="Audio Extractor", layout="centered")
 st.title("🎧 Generator podsumowań wideo i audio 🎧")
@@ -57,19 +56,22 @@ def transcribe_audio(file_path):
 
 
 def download_audio_from_youtube(url):
-    yt = YouTube(url)
-    stream = yt.streams.filter(only_audio=True).first()
-    downloaded_file = stream.download(filename="temp_audio")
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': 'downloaded_audio.%(ext)s',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+        'quiet': True,
+        'no_warnings': True,
+    }
 
-    base, ext = os.path.splitext(downloaded_file)
-    mp3_file = base + ".mp3"
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
 
-    audio = AudioSegment.from_file(downloaded_file)
-    audio.export(mp3_file, format="mp3")
-
-    os.remove(downloaded_file)
-
-    return mp3_file
+    return 'downloaded_audio.mp3'
 
 
 def split_text(text, max_chars=3000):
